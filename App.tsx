e488b5a997dde41bfac6b2a200eac294f9fac587
ahmedsawy1 <sawy22417@gmail.com>
2024-03-17 09:28:11 +0200

    adding font family

--- App.tsx
+++ App.tsx
@@ -1,19 +1,17 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { scale, verticalScale, moderateScale, s, vs, ms } from 'react-native-size-matters';
+import { Fonts } from './src/styles/fonts'
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Image
-        source={{
-          uri: "https://media.geeksforgeeks.org/wp-content/uploads/20231113164232/React-native-copy.webp",
-        }}
-        style={{height: vs(300), width: s(300)}}
-      />
-      <Text style={{paddingTop: ms(50), fontSize: (200)}}>App</Text>
+    <SafeAreaView style={{justifyContent:"center", alignItems:"center", flex:1}}>
+      <Text style={{fontSize: 50, fontFamily: Fonts.hanumanBlack}}>App</Text>
+      <Text style={{fontSize: 50, fontFamily: Fonts.hanumanBold}}>App</Text>
+      <Text style={{fontSize: 50, fontFamily: Fonts.hanumanLight}}>App</Text>
+      <Text style={{fontSize: 50, fontFamily: "Hanuman-Regular"}}>App</Text>
+      <Text style={{fontSize: 50, fontFamily: "Hanuman-Thin"}}>App</Text>
     </SafeAreaView>
-  );
+  )
 }
 
 export default App
