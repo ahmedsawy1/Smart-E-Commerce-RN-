aa3076d7582352e17dcca98dc191f376e60115b3
ahmedsawy1 <sawy22417@gmail.com>
2024-05-08 19:10:02 +0300

    finish yup validation

--- App.tsx
+++ App.tsx
@@ -1,52 +1,74 @@
-import * as yup from 'yup';
-import {useForm} from 'react-hook-form';
 import {Alert, Button, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {useForm} from 'react-hook-form';
+import FormInputController from './src/components/controllers/FormInputController';
 import {yupResolver} from '@hookform/resolvers/yup';
-import FormInput from './src/components/inputs/FormInput';
+import {fromSchema} from './src/constants/schemas/authSchemas';
 
-const formSchema = yup.object({
-  email: yup
-    .string()
-    .email('Please enter a valid email')
-    .required('Email Is Required'),
-  full_name: yup.string().min(3, 'full name must be at least 3 characters'),
-  password: yup.string().min(8, 'Password must be at least 8 characters'),
-});
-
-export default function App() {
-  const {control, handleSubmit} = useForm({
-    defaultValues: {
-      email: '',
-      full_name: '',
-      password: '',
-    },
-    resolver: yupResolver(formSchema),
+const App = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    resolver: yupResolver(fromSchema),
   });
 
-  const onSubmit = data => {
+  const submit = data => {
     console.log(data);
-    Alert.alert('Successful', JSON.stringify(data));
+    Alert.alert(JSON.stringify(data));
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Simple Login Form</Text>
-      <FormInput control={control} name={'email'} placeholder="email" />
-      <FormInput control={control} name={'full_name'} placeholder="full name" />
-      <FormInput
+      <Text>React Hook From Example In React Native</Text>
+
+      <FormInputController
+        control={control}
+        name={'username'}
+        placeholder={'User Name'}
+        errors={errors}
+      />
+
+      <FormInputController
+        control={control}
+        name="email"
+        placeholder="Enter Your Email"
+        errors={errors}
+      />
+
+      <FormInputController
         control={control}
         name={'password'}
-        placeholder="password"
-        secureTextEntry
+        placeholder={'Password'}
+        props={{
+          secureTextEntry: true,
+        }}
+        errors={errors}
       />
-      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+
+      <Button title="submit" onPress={handleSubmit(submit)} />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 2,
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    width: '90%',
+    marginTop: 18,
+    borderColor: 'gray',
+  },
+  textError: {
+    color: 'red',
   },
 });
