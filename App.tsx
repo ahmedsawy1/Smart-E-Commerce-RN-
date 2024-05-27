0af2d06ded15d00b146a90b80b507805cb63d7b0
ahmedsawy1 <sawy22417@gmail.com>
2024-05-28 00:14:02 +0300

    integrate firebase into android

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import SignInScreen from './src/Screens/auth/SigninScreen';
 
 const App = () => {
-  return <SignInScreen />;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        How To Integrate Firebase To React Native
+      </Text>
+    </View>
+  );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: 'black',
+    fontSize: 17,
+  },
+});
